840b6db7e6a2dcd29b350438dfd66c9337d7d928
SoulWone <plentoplay@gmail.com>
2025-01-12 17:11:08 +0300

    Add files via upload

---
+++ static/javaScript/button.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('main-pannel');
+const button = document.getElementById('menuButton');
+button.onclick = function(){
+    if(menu.style.display === "none"){
+        button.innerHTML= '&#x2716;';
+        menu.style.cssText = 'background-color:white; opacity:0.95; position: absolute; top: 3rem; width:100%; display: flex; flex-direction: column;'
+        menu.style.display = "flex";
+        menu.style.flexDirection = "columns";
+    }
+    else{
+        button.innerHTML= '&#9776;';
+        menu.style.display = "none";
+    }
+}
